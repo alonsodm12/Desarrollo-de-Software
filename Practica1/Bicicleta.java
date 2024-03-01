282f631ddd044058b903a08ffc7e51e684c542e5
bete2043 <129316952+bete2043@users.noreply.github.com>
2024-03-01 17:07:59 +0100

    Add files via upload

--- Practica1/Bicicleta.java
+++ Practica1/Bicicleta.java
@@ -4,7 +4,12 @@ package Practica1;
  * Bicicleta
  */
 public abstract class Bicicleta {
+
     
     private int identificador;
+    //jaime
+    Bicicleta(int identificador){
+        this.identificador=identificador;
+    }
     public abstract Bicicleta crearBicicleta();
 }
