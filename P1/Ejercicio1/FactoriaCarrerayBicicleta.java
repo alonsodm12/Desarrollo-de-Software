7b18c6c82a62a073c7c00b98c17823ec9b0276be
alonsodm12 <alonsodmx@gmail.com>
2024-04-08 14:27:53 +0200

    Limpieza

--- P1/Ejercicio1/FactoriaCarrerayBicicleta.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio1;
-/**
- * FactoriaCarrerayBicicleta
- */
-public interface FactoriaCarrerayBicicleta {
-    
-    public Bicicleta crearBicicleta();
-    public Carrera crearCarrera();
-}
