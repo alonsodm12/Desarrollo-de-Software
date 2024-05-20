da82d96f73c63f9b4090d69e5c558255f3ab886f
alonsodm <alonsodmx@gmail.com>
2024-05-20 17:09:49 +0200

    Final estructura

--- P2-P3/ejercicio3/lib/modelo/armadura_decorador.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ejercicio3/modelo/armadura.dart';
-
-abstract class ArmaduraDecorador extends Armadura{
-  
-  Armadura? armaduraDecorada;
-
-  ArmaduraDecorador(this.armaduraDecorada);
-}
